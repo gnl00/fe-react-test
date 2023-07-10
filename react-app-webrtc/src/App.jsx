ab9469d47a3670caaffda3a401dd2dbdac552b9d
liguangning <gnl0@qq.com>
2023-07-10 17:28:47 +0800

    [add] react-app-webrtc

--- react-app-webrtc/src/App.jsx
+++ react-app-webrtc/src/App.jsx
@@ -1,217 +1,89 @@
 import {useEffect} from 'react'
-import adapter from "webrtc-adapter";
-import janus from "./assets/janus.js";
-import Janus from "./assets/janus.js";
 
 import './App.css'
 
-let cliJanus = null
-let clientId = null
-let videoCallPluginHandle = null;
+let localVideoDom = null
+let remoteVideoDom = null
+let localStream = null
+
+let clientId = null;
+
+let ws = null
+let wsURL = 'ws://192.168.2.201:8866/pTop/'
 
 function App() {
 	useEffect(() => {
-		initJanus();
-
-		// let localVideo = document.getElementById('videoLocal');
-		//
-		// navigator.mediaDevices.getDisplayMedia({
-		// 	video: true,
-		// 	audio: false
-		// }).then(stream => {
-		// 	localVideo.srcObject = stream;
-		// }).catch(err => {
-		// 	console.log(err.message)
-		// })
-		//
-		// return () => {
-		// 	localVideo.srcObject = null
-		// }
+		return () => {
+			localVideoDom.srcObject = null
+			remoteVideoDom.srcObject = null
+		}
 	}, [])
 
-
-
-	function initJanus () {
-		janus.init({
-			callback: () => {
-				if (!Janus.isWebrtcSupported()) {
-					console.log('not supported webrtc')
-				}
-			},
-			dependencies: Janus.useDefaultDependencies({adapter})
+	const startVideo = async () => {
+		console.log('startVideo')
+		getDisplay().then(res => {
+			createWebSocket();
 		})
+	}
 
-		cliJanus = new Janus({
-			server: 'http://192.168.115.140:8088/janus',
-			success: () => {
-				console.log('new Janus success')
-				initVideoCallPlugin();
-			},
-			error: err => {
-				console.log(err)
-			},
-			destroy: () => {
-				console.log('destroy')
+	function createWebSocket() {
+		if (!clientId) return
+
+		ws = new WebSocket(wsURL + clientId)
+		console.log(ws)
+
+		websocketInit(ws);
+	}
+
+	function websocketInit(webSocket) {
+		if (!webSocket) return
+
+		webSocket.onopen = (evt) => {
+			console.log('websocket opened ', evt)
+		}
+
+		webSocket.onclose = (evt) => {
+			console.log('websocket closed ', evt)
+		}
+
+		webSocket.onerror = (evt) => {
+			console.log('websocket error:  ', evt)
+		}
+
+		webSocket.onmessage = (evt) => {
+			console.log('websocket onmessage:  ', evt)
+			if (evt && evt.data) {
+				// messageTypeHandler(JSON.parse(evt.data))
+			}
+
+		}
+	}
+
+	function getDisplay() {
+		localVideoDom = document.getElementById('videoLocal');
+		return navigator.mediaDevices.getDisplayMedia({
+			video: true, audio: false
+		}).then(stream => {
+			clientId = stream.id
+			if (stream) {
+				localStream = stream
+				localVideoDom.srcObject = stream
+
+				// 设置视频播放音量为 0
+				localVideoDom.volume = 0
+				// 设置视频自动播放
+				localVideoDom.autoplay = true
 			}
 		})
-
 	}
 
-	const initVideoCallPlugin = () => {
-		console.log('initVideoCallPlugin')
-
-		// client unique Id
-		clientId = Janus.randomString(8);
-
-		cliJanus.attach({
-			opaqueId: clientId,
-			plugin: 'janus.plugin.videocall',
-			success: pluginHandle => {
-				videoCallPluginHandle = pluginHandle
-
-				setInterval(() => {
-					getBitrate()
-				}, 1500)
-
-				console.log('janus.plugin.videocall initialized')
-			},
-			error: (cause) => {
-				console.log(cause)
-			},
-			onmessage: (msg, jsep) => {
-				// msg 交互信息：create/join/stop
-				// jsep：协商信令
-				onMessage(msg, jsep)
-			},
-			onlocaltrack: (track, added) => {
-				console.log('onlocaltrack')
-				if (added) {
-					setVideoTrack('localVideo', track)
-				}
-			},
-			onremotetrack: (track, mid, added) => {
-				console.log('onremotetrack')
-
-				if (added) {
-					setVideoTrack('remoteVideo', track)
-				}
-			},
-			oncleanup: () => {},
-			detach: () => {}
-		})
-	}
-
-	const getBitrate = () => {
-		if (videoCallPluginHandle) {
-			console.log(videoCallPluginHandle)
-			// console.log(videoCallPluginHandle.getBitrate())
-		}
-	}
-
-	const onMessage = (msg, jsep) => {
-		console.log('::: Received Message :::')
-		console.log(msg)
-		const result = msg['result']
-		if (result) {
-			if (result["list"]) {
-				let list = result["list"];
-				console.log("注册Peers", list)
-			} else if (result["event"]) {
-				let event = result["event"];
-				if (event === 'registered') {
-					console.log("注册成功", msg)
-					console.log("注册成功")
-					videoCallPluginHandle.send({message: {request: "list"}});
-				} else if (event === 'calling') {
-					console.log("呼叫中")
-					console.log("呼叫中，请稍后")
-				} else if (event === 'incomingcall') {
-					let username = result["username"]
-					console.log("来自于 【" + username + "】的呼叫")
-					videoCallPluginHandle.createAnswer({
-						jsep: jsep,
-						tracks: [
-							{type: 'audio', capture: true, recv: true},
-							{type: 'video', capture: true, recv: true},
-							{type: 'data'},
-						],
-						success: function (jsep) {
-							Janus.debug("应答 SDP!", jsep);
-							let body = {request: "accept"};
-							videoCallPluginHandle.send({message: body, jsep: jsep});
-						},
-						error: function (error) {
-							console.error("创建应答异常", error)
-						}
-					});
-				} else if (event === 'accepted') {
-					console.log("对方已接听同时设置协商信息", jsep)
-					if (jsep) {
-						videoCallPluginHandle.handleRemoteJsep({jsep: jsep});
-					}
-					console.log("对方已接听")
-				} else if (event === 'update') {
-					// An 'update' event may be used to provide renegotiation attempts
-					if (jsep) {
-						if (jsep.type === "answer") {
-							videoCallPluginHandle.handleRemoteJsep({jsep: jsep});
-						} else {
-							videoCallPluginHandle.createAnswer({
-								jsep: jsep,
-								tracks: [
-									{type: 'audio', capture: true, recv: true},
-									{type: 'video', capture: true, recv: true},
-									{type: 'data'},
-								],
-								success: function (jsep) {
-									console.log("重新应答信令 SDP!", jsep);
-									var body = {request: "set"};
-									videoCallPluginHandle.send({message: body, jsep: jsep});
-								},
-								error: function (error) {
-									console.error(error)
-								}
-							});
-						}
-					}
-				} else if (event === 'hangup') {
-					console.log(result["username"] + "已挂断,原因:(" + result["reason"] + ")!");
-					videoCallPluginHandle.hangup();
-					console.log("已挂断")
-
-				} else if (event === "simulcast") {
-					console.log("联播simulcast，暂时不用考虑", msg)
-				}
-			}
-		} else {
-			const error = msg['error']
-			console.log(error)
-
-			videoCallPluginHandle.hangup();
-		}
-	}
-
-	const setVideoTrack = (videoDomId, track) => {
-		const videoDom = document.getElementById(videoDomId);
-		let stream = videoDom.srcObject
-		if (stream) {
-			stream.addTrack(track)
-		} else {
-			stream = new MediaStream();
-			stream.addTrack(track)
-			videoDom.srcObject = stream
-			videoDom.controls = false
-			videoDom.autoplay = true
-		}
-	}
-
-	return (
-		<>
+	return (<>
 			<div>
+				<button onClick={startVideo}>click</button>
 				<video id={"videoLocal"} autoPlay muted width={"720px"} height={"640px"}></video>
+				<video id={"videoRemote"} autoPlay muted width={"720px"} height={"640px"}></video>
 			</div>
-		</>
-	)
+		</>)
 }
 
 export default App
